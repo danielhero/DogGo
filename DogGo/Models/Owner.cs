4de91a33da433fd417008aa1713a0e0740ed31e0
Daniel Hero <danielhero@outlook.com>
2020-06-18 11:53:13 -0500

    changed owner display and form

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -14,6 +14,5 @@ namespace DogGo.Models
         public int NeighborhoodId { get; set; }
         public Neighborhood Neighborhood { get; set; }
         public string Phone { get; set; }
-        public List<Dog> Dogs { get; set; }
     }
 }
