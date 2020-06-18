c45e436dd8d7df13e2ae6ae08bce563c45740132
Daniel Hero <danielhero@outlook.com>
2020-06-18 15:38:08 -0500

    added walks model and repo

---
+++ DogGo/Repositories/WalksRepository.cs
@@ -0,0 +1,63 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Repositories
+{
+    public class WalksRepository
+    {
+        private readonly IConfiguration _config;
+
+        public WalksRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Walks> GetAllWalks()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, Date, Duration, WalkerId, DogId
+                        FROM Walks
+                    ";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Walks> allWalks = new List<Walks>();
+                    while (reader.Read())
+                    {
+                        Walks walk = new Walks
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Date = reader.GetDateTime(reader.GetOrdinal("Date")),
+                            Duration = reader.GetInt32(reader.GetOrdinal("Duration")),
+                            WalkerId = reader.GetInt32(reader.GetOrdinal("WalkerId")),
+                            DogId = reader.GetInt32(reader.GetOrdinal("DogId"))
+                        };
+
+                        allWalks.Add(walk);
+                    }
+
+                    reader.Close();
+
+                    return allWalks;
+                }
+            }
+        }
+}
