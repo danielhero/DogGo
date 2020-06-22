2d4cce74dce883d90ad3c9a5f3df224d51497545
Daniel Hero <danielhero@outlook.com>
2020-06-22 13:09:42 -0500

    added annotations

--- DogGo/Models/Walker.cs
+++ DogGo/Models/Walker.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +10,16 @@ namespace DogGo.Models
     public class Walker
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Image")]
         public string ImageUrl { get; set; }
+
+        [Required]
+        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
         public Neighborhood Neighborhood { get; set; }
     }
